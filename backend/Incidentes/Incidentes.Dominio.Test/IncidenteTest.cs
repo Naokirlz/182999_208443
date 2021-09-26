14efeb01df22f62bb6f23494564829e1367adfb9
Federico Alonso <46576525+federicoalonso@users.noreply.github.com>
2021-09-26 02:24:00 +0200

    Se crea clase Incidente

---
+++ backend/Incidentes/Incidentes.Dominio.Test/IncidenteTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using Incidentes.Dominio;
+
+namespace Incidentes.Dominio.Test
+{
+    public class IncidenteTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void se_puede_dar_nombre_a_un_incidente()
+        {
+            Incidente unIncidente = new Incidente()
+            {
+                Nombre = "Incidente complicado"
+            };
+            Assert.AreEqual("Incidente complicado", unIncidente.Nombre);
+        }
+    }
+}
---
+++ backend/Incidentes/Incidentes.Dominio/Incidente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Incidentes.Dominio
+{
+    public class Incidente
+    {
+        public string Nombre { get; set; }
+
+        public Incidente() { }
+    }
+}
