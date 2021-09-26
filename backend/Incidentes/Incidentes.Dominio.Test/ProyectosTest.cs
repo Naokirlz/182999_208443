fd0579049d2ce816bad5a8ffeb68151abfda7a2a
Federico Alonso <46576525+federicoalonso@users.noreply.github.com>
2021-09-26 02:16:27 +0200

    Se crea clase proyecto

---
+++ backend/Incidentes/Incidentes.Dominio.Test/ProyectosTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using Incidentes.Dominio;
+
+namespace Incidentes.Dominio.Test
+{
+    public class ProyectosTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void se_puede_dar_nombre_a_un_proyecto()
+        {
+            Proyecto unProyecto = new Proyecto()
+            {
+                Nombre = "Trabajo conjunto"
+            };
+            Assert.AreEqual("Trabajo conjunto", unProyecto.Nombre);
+        }
+    }
+}
---
+++ backend/Incidentes/Incidentes.Dominio/Proyecto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Incidentes.Dominio
+{
+    public class Proyecto
+    {
+        public string Nombre { get; set; }
+
+        public Proyecto() { }
+
+    }
+}
