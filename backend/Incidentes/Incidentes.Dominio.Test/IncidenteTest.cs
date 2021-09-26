d27d539a1692e2ca608cccb80f4675c8182bdbcd
Federico Alonso <46576525+federicoalonso@users.noreply.github.com>
2021-09-26 02:50:30 +0200

    Se crean Incidentes

--- backend/Incidentes/Incidentes.Dominio.Test/IncidenteTest.cs
+++ backend/Incidentes/Incidentes.Dominio.Test/IncidenteTest.cs
@@ -19,5 +19,54 @@ namespace Incidentes.Dominio.Test
             };
             Assert.AreEqual("Incidente complicado", unIncidente.Nombre);
         }
+
+        [Test]
+        public void se_puede_asignar_a_un_incidente_un_proyecto()
+        {
+            Incidente unIncidente = new Incidente()
+            {
+                NombreProyecto = "Proyecto uno"
+            };
+            Assert.AreEqual("Proyecto uno", unIncidente.NombreProyecto);
+        }
+
+        [Test]
+        public void se_puede_dar_descripcion_a_un_incidente()
+        {
+            Incidente unIncidente = new Incidente()
+            {
+                Descripcion = "descripcion de incidente"
+            };
+            Assert.AreEqual("descripcion de incidente", unIncidente.Descripcion);
+        }
+        [Test]
+        public void se_puede_dar_version_a_un_incidente()
+        {
+            Incidente unIncidente = new Incidente()
+            {
+                Version = 1
+            };
+            Assert.AreEqual(1, unIncidente.Version);
+        }
+
+        [Test]
+        public void se_incidente_puede_estar_activo()
+        {
+            Incidente unIncidente = new Incidente()
+            {
+                EstadoIncidente = Incidente.Estado.Activo
+            };
+            Assert.AreEqual(Incidente.Estado.Activo, unIncidente.EstadoIncidente);
+        }
+
+        [Test]
+        public void se_incidente_puede_estar_resuelto()
+        {
+            Incidente unIncidente = new Incidente()
+            {
+                EstadoIncidente = Incidente.Estado.Resuelto
+            };
+            Assert.AreEqual(Incidente.Estado.Resuelto, unIncidente.EstadoIncidente);
+        }
     }
 }
--- backend/Incidentes/Incidentes.Dominio/Incidente.cs
+++ backend/Incidentes/Incidentes.Dominio/Incidente.cs
@@ -9,7 +9,17 @@ namespace Incidentes.Dominio
     public class Incidente
     {
         public string Nombre { get; set; }
+        public string NombreProyecto { get; set; }
+        public string Descripcion { get; set; }
+        public int Version { get; set; }
+        public Estado EstadoIncidente { get; set; }
 
         public Incidente() { }
+
+        public enum Estado
+        {
+            Activo,
+            Resuelto
+        }
     }
 }
