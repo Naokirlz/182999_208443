e5a49c12bbdfad0560e3377684be177df9346963
Federico Alonso <46576525+federicoalonso@users.noreply.github.com>
2021-10-02 20:49:02 +0200

    Funcionalidad: Se pueden importar bugs

---
+++ backend/Incidentes/Incidentes.WebApi/Controllers/ImportacionesController.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using Incidentes.Dominio;
+using Incidentes.Logica.Interfaz;
+using Incidentes.LogicaInterfaz;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace Incidentes.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImportacionesController : ControllerBase
+    {
+        private const string error_de_servidor = "Internal Server Error";
+        private readonly IMapper _mapper;
+        private readonly ILogicaProyecto _logica;
+
+        public ImportacionesController(ILogicaProyecto logica, IMapper mapper)
+        {
+            _logica = logica;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] string rutaFuente)
+        {
+            try
+            {
+                _logica.ImportarBugs(rutaFuente);
+            }
+            catch (ArgumentNullException nullex)
+            {
+                return UnprocessableEntity(nullex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, error_de_servidor);
+            }
+            return Ok();
+        }
+    }
+}
---
+++ backend/Incidentes/Incidentes.WebApiTest/ImportacionesControllerTest.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using Incidentes.Dominio;
+using Incidentes.LogicaInterfaz;
+using Incidentes.WebApi.Controllers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Incidentes.WebApiTest
+{
+    public class ImportacionesControllerTest
+    {
+        private Mock<ILogicaProyecto> _logicaP;
+        private Mock<IMapper> _mapper;
+        private ImportacionesController _iController;
+
+        [SetUp]
+        public void Setup()
+        {
+            _logicaP = new Mock<ILogicaProyecto>();
+            _mapper = new Mock<IMapper>();
+            _iController = new ImportacionesController(_logicaP.Object, _mapper.Object);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _logicaP = null;
+            _mapper = null;
+            _iController = null;
+        }
+
+        [Test]
+        public void se_pueden_importar_bugs()
+        {
+            _logicaP.Setup(c => c.ImportarBugs(It.IsAny<string>()));
+
+            var result = _iController.Post("ruta");
+
+            Assert.IsNotNull(result);
+
+            _logicaP.Verify(c => c.ImportarBugs(It.IsAny<string>()));
+        }
+    }
+}
