5612230c4dee2841cfbea49d0594fe65fc27178a
Federico Alonso <46576525+federicoalonso@users.noreply.github.com>
2021-10-05 23:55:12 +0200

    Creado Excepciones

---
+++ backend/Incidentes/Incidentes.WebApi/Filters/TrapExcepciones.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Incidentes.Logica.Excepciones;
+
+namespace Incidentes.WebApi.Filters
+{
+    public class TrapExcepciones : Attribute, IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            int statusCode = 500;
+            string content = context.Exception.Message;
+
+            var type = context.Exception.GetType();
+            if (type == typeof(ExcepcionAccesoNoAutorizado))
+            {
+                statusCode = 401; 
+            }
+            else if (type == typeof(ExcepcionArgumentoNoValido))
+            {
+                statusCode = 400; 
+            }
+            else if (type == typeof(ExcepcionElementoNoExiste))
+            {
+                statusCode = 400; 
+            }
+            else if (type == typeof(ExcepcionLargoTexto))
+            {
+                statusCode = 400; 
+            }
+            else
+            {
+                content = "Internal Server Error | " + context.Exception.Message;
+            }
+
+            context.Result = new ContentResult()
+            {
+                StatusCode = statusCode,
+                Content = content
+            };
+        }
+    }
+}
