f432f29fd5b0018c8ec49ed9e003be9d6d4d81ee
Federico Alonso <46576525+federicoalonso@users.noreply.github.com>
2021-11-13 14:27:17 -0800

    Refactor: Se desacopla completamente importacion JSON

--- Codigo Fuente/Incidentes/Incidentes.ImportacionesJSON/ImportacionJSON.cs
+++ Codigo Fuente/Incidentes/Incidentes.ImportacionesJSON/ImportacionJSON.cs
@@ -1,13 +1,10 @@
-﻿using Incidentes.Dominio;
-using Incidentes.LogicaInterfaz;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Text.Json;
 
 namespace Incidentes.ImportacionesJSON
 {
-    public class ImportacionJSON : IFuente
+    public class ImportacionJSON
     {
         public List<Proyecto> ImportarBugs(string rutaFuente)
         {
@@ -20,5 +17,30 @@ namespace Incidentes.ImportacionesJSON
             retorno.Add(pro);
             return retorno;
         }
+    }
+
+    public class Incidente
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string Version { get; set; }
+        public Estado EstadoIncidente { get; set; }
+
+        public Incidente() { }
+
+        public enum Estado
+        {
+            Indiferente,
+            Activo,
+            Resuelto
+        }
+    }
+
+    public class Proyecto
+    {
+        public string Nombre { get; set; }
+        public List<Incidente> Incidentes { get; set; }
+        public Proyecto() { }
     }
 }
