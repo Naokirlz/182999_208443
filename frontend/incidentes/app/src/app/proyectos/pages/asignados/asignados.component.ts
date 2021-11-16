ed8a350b6e3cabeb1686e9cf1a296c010c18f314
Naokirlz <cristian_pd88@hotmail.com>
2021-11-16 15:56:55 -0300

    comienza documentacion

--- frontend/incidentes/app/src/app/proyectos/pages/asignados/asignados.component.ts
+++ frontend/incidentes/app/src/app/proyectos/pages/asignados/asignados.component.ts
@@ -17,16 +17,16 @@ export class AsignadosComponent implements OnInit {
 
 
   public proyectoId: number;
-  public proyectos:Proyecto[]=[];
-  public usuarios:Usuario[]=[];
-  public asignados:Usuario[] | undefined=[];
-  
+  public proyectos: Proyecto[] = [];
+  public usuarios: Usuario[] = [];
+  public asignados: Usuario[] | undefined = [];
+
 
   constructor(private _route: ActivatedRoute,
-              private proyectoService:ProyectoService,
-              private _router: Router,
-              private usuarioServie:UsuariosService,
-              private messageService: MessageService) { 
+    private proyectoService: ProyectoService,
+    private _router: Router,
+    private usuarioServie: UsuariosService,
+    private messageService: MessageService) {
 
     this.proyectoId = 0;
 
@@ -36,11 +36,11 @@ export class AsignadosComponent implements OnInit {
 
     this.proyectoId = this._route.snapshot.params['proyectoId'];
     //let numeroRol : number = parseInt(this.proyectoId);
-    
+
     this.proyectoService.getBy(this.proyectoId)
-    .subscribe(
-      ((data: Proyecto) => this.result(data)),
-    );
+      .subscribe(
+        ((data: Proyecto) => this.result(data)),
+      );
 
   }
 
@@ -50,77 +50,74 @@ export class AsignadosComponent implements OnInit {
     { label: 'Asignados' },
   ];
 
-  eliminar(id:number){
+  eliminar(id: number) {
 
     this.asignados = this.asignados!.filter(p => p.id !== id);
 
     this.usuarioServie.getBy(id)
-    .subscribe(
-      ((data: Usuario) => this.usuarios?.push(data)),
-    );
+      .subscribe(
+        ((data: Usuario) => this.usuarios?.push(data)),
+      );
 
   }
-  
+
   private result(data: Proyecto): void {
     this.proyectos.push(data);
     this.asignados = this.proyectos[0].asignados;
 
     this.usuarioServie.getUsuario()
-    .subscribe(
-      ((data: Array<Usuario>) => this.result2(data)),
-    );
-    
+      .subscribe(
+        ((data: Array<Usuario>) => this.result2(data)),
+      );
+
   }
 
   private result2(data: Array<Usuario>): void {
-   
-   this.usuarios = data;
-   this.asignados?.forEach((e)=>{
-   this.usuarios = this.usuarios.filter(p => p.id !== e.id);
-   this.usuarios = this.usuarios.filter(p => p.rolUsuario !== 0);
 
-   })
-   
-
-  }
-  
-  agregar(id:number){
-
-  this.usuarios = this.usuarios!.filter(p => p.id !== id);
- 
-  this.usuarioServie.getBy(id)
-  .subscribe(
-    ((data: Usuario) => this.asignados?.push(data)),
-  );
- 
+    this.usuarios = data;
+    this.usuarios = this.usuarios.filter(p => p.rolUsuario !== 0);
+    this.asignados?.forEach((e) => {
+      this.usuarios = this.usuarios.filter(p => p.id !== e.id);
+    })
   }
 
-  actualizar(){
+  agregar(id: number) {
 
-    this.proyectos[0].asignados=this.asignados;
+    this.usuarios = this.usuarios!.filter(p => p.id !== id);
+
+    this.usuarioServie.getBy(id)
+      .subscribe(
+        ((data: Usuario) => this.asignados?.push(data)),
+      );
+
+  }
+
+  actualizar() {
+
+    this.proyectos[0].asignados = this.asignados;
     this.proyectoService.update(this.proyectos[0])
       .subscribe((data: Proyecto) => {
         this.messageService.add({
           severity: 'success', summary: 'Listo',
           detail: 'Usuario guardado correctamente.'
         });
-          this.volver();
+        this.volver();
       },
         (({ error }: any) => {
-          this.messageService.add({ severity: 'error', summary: 'Error', detail: error});
+          this.messageService.add({ severity: 'error', summary: 'Error', detail: error });
         }
         )
       );
 
-    
-    
-    
+
+
+
     this.volver();
 
-}
+  }
 
 
-  volver(){
+  volver() {
 
     this._router.navigate([`/proyectos`]);
 
